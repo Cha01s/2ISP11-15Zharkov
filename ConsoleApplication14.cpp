52879cf0084e841f73b0802ec597dc93a6d610cf
Alexey <HangedMan1666@mail.ru>
2021-11-26 03:08:57 +0300

    oohh

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,124 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    const int SIZE = 10;
+    int massive = 0;
+
+
+    cout << "[+]Список массивов:" << endl;
+
+    cout << "[1]Массив int" << endl;
+    int first[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    cout << "[2]Массив string" << endl;
+    string second[SIZE] = { "asd", "qwe", "zxc", "rty", "tyu", "yui", "vbn", "cvb", "iop", "ghj" };
+
+    cout << "[3]Массив float" << endl;
+    float third[SIZE] = { 56.1, 123.243, 27.573, 35.44, 45.5, 557.67, 677.75, 7.8, 84.95, 95.11 };
+
+    cout << "[4]Массив double" << endl;
+    double fourth[SIZE] = { 0456.1123, 134.2123123, 243.3123123, 343.434543, 44.534543, 534.6345435, 64.735445, 74.834553, 843.93324, 934.23432411 };
+
+    cout << "[5]Массив char" << endl;
+    char fifth[SIZE] = { 'a', 'q', 'z', 'r', 't', 'y', 'v', 'c', 'i', 'g' };
+
+    cout << "[6]Массив short" << endl;
+    short sixth[SIZE] = { 55, 71, 22, 33, 44, 55, 67, 78, 86, 94 };
+
+    cout << "[7]Массив long" << endl;
+    long seventh[SIZE] = { 55345, 71567, 23452, 31233, 45674, 55675, 68907, 78908, 88906, 98904 };
+
+    cout << "[8]Массив bool" << endl;
+    bool eighth[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+    cout << "[+]Выберите массив: ";
+    cin >> massive;
+
+
+
+    switch (massive)
+    {
+    case 1:
+        system("cls");
+        cout << "[+]Массив int:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << first[i] << endl;
+        }
+        break;
+
+    case 2:
+        system("cls");
+        cout << "[+]Массив string:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << second[i] << endl;
+        }
+        break;
+
+    case 3:
+        system("cls");
+        cout << "[+]Массив float:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << third[i] << endl;
+        }
+        break;
+
+    case 4:
+        system("cls");
+        cout << "[+]Массив double:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << fourth[i] << endl;
+        }
+        break;
+
+    case 5:
+        system("cls");
+        cout << "[+]Массив char:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << fifth[i] << endl;
+        }
+        break;
+
+    case 6:
+        system("cls");
+        cout << "[+]Массив short:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << fifth[i] << endl;
+        }
+        break;
+
+    case 7:
+        system("cls");
+        cout << "[+]Массив long:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << seventh[i] << endl;
+        }
+        break;
+
+    case 8:
+        system("cls");
+        cout << "[+]Массив bool:" << endl;
+        for (int i = 0; i < SIZE; i++)
+        {
+            cout << eighth[i] << endl;
+        }
+        break;
+
+    default:
+        system("cls");
+        cout << "Выбранного массива нет в списке.";
+    }
+
+    int _;
+    cin >> _;
+}
---
+++ ConsoleApplication8-13.cpp
@@ -0,0 +1,294 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int figure, type, length, size, hight = 0;
+	int count = 1;
+	char texture = ' ';
+
+	cout << "[+]Программа - \"Геометрические фигуры\"" << endl;
+	cout << "[1]Линия" << endl;
+	cout << "[2]Квадрат" << endl;
+	cout << "[3]Прямоугольник" << endl;
+	cout << "[4]Прямоугольник пустой" << endl;
+	cout << "[5]Квадрат пустой" << endl;
+	cout << "[6]Треугольник пустой" << endl;
+	cout << "[7]Треугольник" << endl;
+	cout << "[8]Круг" << endl;
+	cout << "[9]Крест" << endl;
+	cout << "[10]Квадрат в квадрате" << endl;
+    cout << "[+]Выберите фигуру: " << endl;
+	cin >> figure;
+
+	switch (figure)
+	{
+	case 1:
+		system("cls");
+		cout << "[+]Выберите тип: " << endl;
+		cout << "[1]Горизонтальная" << endl;
+		cout << "[2]Вертикальная" << endl;
+		cin >> type;
+
+		switch (type)
+		{
+		case 1:
+			cout << "[+]Текстура линии: " << endl;
+			cin >> texture;
+			system("cls");
+			cout << "[+]Длинна линии: " << endl;
+			cin >> length;
+
+			system("cls");
+			while (count < length + 1)
+			{
+				cout << texture;
+				count++;
+			}
+
+		case 2:
+			cout << "[+]Текстура линии: " << endl;
+			cin >> texture;
+			cout << "[+]Длинна линии: " << endl;
+			cin >> length;
+
+			system("cls");
+			while (count < length + 1)
+			{
+				cout << texture << endl;
+				count++;
+			}
+			break;
+		default:
+			cout << "[+]Указанного типа нет в списке.";
+		}
+		break;
+	default:
+		cout << "[+]Указанной фигуры нет в списке.";
+		break;
+	case 2:
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				cout << texture;
+			}
+			cout << endl;
+
+		}
+		break;
+	case 3:
+		system("cls");
+		cout << "[+]Длина: " << endl;
+		cin >> length;
+		cout << "[+]Высота: " << endl;
+		cin >> hight;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+
+		for (int y = 0; y < hight; y++) {
+			for (int x = 0; x < length; x++) {
+				cout << texture;
+			}
+			cout << endl;
+
+		}
+		break;
+	case 4:
+		system("cls");
+		cout << "[+]Длина: " << endl;
+		cin >> length;
+		cout << "[+]Высота: " << endl;
+		cin >> hight;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+		for (int x = 0; x < length; x++) {
+			cout << texture;
+		}
+		cout << endl;
+		for (int y = 0; y < hight; y++) {
+			cout << texture;
+			for (int x = 0; x < length - 2; x++) {
+				cout << " ";
+			}
+			cout << texture << endl;
+
+		}
+		for (int x = 0; x < length; x++) {
+			cout << texture;
+		}
+		break;
+	case 5:
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+		for (int y = 0; y < size; y++)
+		{
+			for (int x = 0; x < size; x++)
+			{
+				if (y == 0 || y == size - 1 || x == 0 || x == size - 1)
+				{
+					cout << texture;
+				}
+				else
+				{
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+	case 6:
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+		for (int y = 0; y < size; y++)
+		{
+			for (int x = 0; x < size; x++)
+			{
+				if (x == size / 2 - y || x == size / 2 + y || y == size / 2)
+				{
+					cout << texture;
+				}
+				else
+				{
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+	case 7:
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+		for (int y = 0; y < size; y++)
+		{
+			for (int x = 0; x < size; x++)
+			{
+				if (x >= size / 2 - y && x <= size / 2 + y && y <= size / 2)
+				{
+					cout << texture;
+				}
+				else
+				{
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+	case 8:
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+		for (int y = -size; y <= size; y++)
+		{
+			for (int x = size; x >= -size; x--)
+			{
+				if (x * x + y * y == size * size)
+				{
+					cout << texture << " ";
+				}
+				else
+				{
+					cout << "  ";
+				}
+			}
+			cout << endl;
+		}
+		break;
+	case 9:
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+		for (int y = 0; y < size; y++)
+		{
+			for (int x = 0; x < size; x++)
+			{
+				if (x == size / 2 || y == size / 2)
+				{
+					cout << texture;
+				}
+				else
+				{
+					cout << " ";
+				}
+			}
+			cout << endl;
+		}
+
+	case 10:
+
+		system("cls");
+		cout << "[+]Размер: " << endl;
+		cin >> size;
+		cout << "[+]Текстура: " << endl;
+		cin >> texture;
+		system("cls");
+
+		for (int y = 0; y < size; y++)
+		{
+			for (int x = 0; x < size; x++)
+			{
+				for (int interval = 0; interval < size / 2; interval = interval + 2)
+				{
+					if ((size - 1 - interval) >= x && x >= interval && (y == interval || y == size - 1 - interval))
+					{
+						cout << texture;
+					}
+					else if ((size - 1 - interval) >= y && y >= interval && (x == interval || x == size - 1 - interval))
+					{
+						cout << texture;
+					}
+					else if ((size - 2 - interval) >= x && x >= interval + 1 && (y == interval + 1 || y == size - 2 - interval))
+					{
+						cout << " ";
+					}
+					else if ((size - 2 - interval) >= y && y >= interval + 1 && (x == interval + 1 || x == size - 2 - interval))
+					{
+						cout << " ";
+					}
+
+				}
+			}
+			cout << endl;
+		}
+	}
+
+
+
+
+
+	int _;
+	cin >> _;
+}
